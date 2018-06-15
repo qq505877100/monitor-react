e713180611bcfc5498fe7ba5a55df8269a813124
HakunaMatataLy <915051280@qq.com>
2018-06-15 16:46:37 +0800

    ssssss

--- src/components/yf/api.jsx
+++ src/components/yf/api.jsx
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import echarts from 'echarts/lib/echarts';
 import { bar, tooltip, title } from 'echarts';
+import { getNum } from '../../reducers/yf/yf.reducer'
+import { connect } from 'react-redux'
 import axios from 'axios';
 
 const url = "http://192.168.51.199:8090/assert/test/test";
-
+@connect(
+    state => state.yfReducer,//要哪些状态
+    { getNum }//需要什么动作
+)
 export default class Api extends React.Component {
     constructor(props) {
-        console.log("constructor",props)
         super(props);
     }
 
     //侧边栏点击修改props渲染数据
     componentWillReceiveProps(props) {
-        console.log("componentWillReceiveProps",props)
+        let user = this.props.cey
+        let model = this.props.selected
+        console.log("componentWillReceiveProps", props)
+        // 基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('api'), 'dark');
+        // 绘制图表
         myChart.setOption({
             title: { text: 'ECharts' },
             tooltip: {},
@@ -32,37 +40,10 @@ export default class Api extends React.Component {
 
     //组件加载完毕去请求数据
     componentDidMount() {
-        console.log("componentDidMount", this.props)
         //請求參數
-        let data = {
-            user: this.props.selected,
-            model: this.props.cey
-        }
-        let that = this;
-        axios.post(url, data)
-            .then(function (response) {
-                console.log(response.data)
-                // 基于准备好的dom，初始化echarts实例
-                var myChart = echarts.init(document.getElementById('api'), 'dark');
-                // 绘制图表
-                myChart.setOption({
-                    title: { text: 'ECharts' },
-                    tooltip: {},
-                    xAxis: {
-                        data: Object.keys(response.data)
-                    },
-                    yAxis: {},
-                    series: [{
-                        name: '测试',
-                        type: 'bar',
-                        data: Object.values(response.data)
-                    }]
-                });
-            })
-            .catch(function (error) {
-                console.log('error----------' + error);
-            });
-
+        let user = this.props.selected
+        let model = this.props.cey
+        this.props.getNum({ user, model })
     }
     render() {
         let style = {
