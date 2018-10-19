95b6e7f3ed014a54cf70f65bff1c8f13907c2df6
HakunaMatataLy <915051280@qq.com>
2018-10-19 14:51:42 +0800

    爸爸提交

--- src/components/product/userAccess.jsx
+++ src/components/product/userAccess.jsx
@@ -515,7 +515,9 @@ export default class UserAccess extends Component {
 
                         {/* 统计图表 */}
                         <div className="fxh-header-chart">
-                            <ReactEcharts onEvents={onEvents} style={{ width: '950px', height: '300px', margin: "0 auto" }} option={this.getOption()} />
+                            <ReactEcharts onEvents={onEvents} 
+                                style={{ width: '950px', height: '300px', margin: "0 auto" }} 
+                                option={this.getOption()} />
                         </div>
                     </div>
 
